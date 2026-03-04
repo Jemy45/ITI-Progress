fe22468b05864169be7f656145bbf5207db4e85d
Ahmed Mohamed <agml2047@gmail.com>
2026-03-05 01:12:43 +0200

    Run the website on main host

---
+++ Docker/Tasks/Task 2/run_flask.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Navigate to the task2 directory
+cd "/media/jimmy/Data/ITIContent/ITIRepo/ITI-Progress/Docker/Tasks/Task 2"
+
+# Clone the repository
+echo "Cloning repository..."
+git clone https://github.com/Jemy45/basic-flask-app
+
+# Navigate to the application directory
+cd basic-flask-app
+# Create virtual environment
+echo "Creating virtual environment..."
+python3 -m venv venv
+
+# Activate virtual environment
+echo "Activating virtual environment..."
+source venv/bin/activate
+
+# Install requirements
+echo "Installing requirements..."
+pip install -r requirements.txt
+
+# Run the Flask application
+echo "Starting Flask application..."
+python routes.py
